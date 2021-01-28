e8373003ad332b53b071dc23b75bda93778ebb5b
Rahne Sinclair <rahnesinclair97@gmail.com>
2021-01-28 12:39:31 -0300

    feat: reducing bridge contract size, modified migrations

---
+++ sovryn-token-bridge/bridge/contracts/Auth.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+import "./zeppelin/cryptography/ECDSA.sol";
+
+contract Auth {
+    using ECDSA for bytes32;
+
+    /**
+     * Verify that msgHash is signed by signerAddr
+     */
+    function verifySignature(address signerAddr, bytes calldata msgHash, bytes calldata signature) external pure {
+        address retAddr = keccak256(msgHash).toEthSignedMessageHash().recover(signature);
+        require(retAddr == signerAddr, "Bridge: The signature is not valid!");
+    }
+
+}
