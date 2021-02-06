5d82d44ba4744df58cb8965172b3d2d3118f089a
Rahne Sinclair <rahnesinclair97@gmail.com>
2021-02-06 11:43:32 -0300

    fix: fixing migration, changing bridge federation address

--- sovryn-token-bridge/bridge/migrations/18_deploy_federation_v2.js
+++ sovryn-token-bridge/bridge/migrations/18_deploy_federation_v2.js
@@ -1,6 +1,7 @@
 const Federation = artifacts.require("Federation");
 const Bridge = artifacts.require("Bridge");
 const MultiSigWallet = artifacts.require("MultiSigWallet");
+const proxyAdminAbi = require('../../abis/ProxyAdmin.json');
 
 module.exports = function(deployer, networkName, accounts) {
     deployer
@@ -10,8 +11,25 @@ module.exports = function(deployer, networkName, accounts) {
             const multiSig = await MultiSigWallet.deployed();
             const bridge = await Bridge.deployed();
 
+            if (networkName === 'soliditycoverage') {
+                return bridge;
+            }
+
+            let jsonName = networkName;
+            const chainId = await web3.eth.net.getId();
+            if((chainId >= 30 && chainId <=33) || chainId == 5777) {
+                jsonName = `dev-${chainId}`;
+            }
+
+            const networkConfig = require(`../.openzeppelin/${jsonName}.json`);
+
+            const proxyAdminAddress = networkConfig.proxyAdmin.address;
+            const proxyAdmin = new web3.eth.Contract(proxyAdminAbi, proxyAdminAddress);
+
             await federation.setBridge(bridge.address);
             await federation.transferOwnership(multiSig.address);
 
+            const changeFederationData = bridge.contract.methods.changeFederation(federation.address).encodeABI();
+            await multiSig.submitTransaction(proxyAdmin.options.address, 0, changeFederationData, { from: accounts[0] });
         });
 };
