01d49a4a819fa5309c4a9741c2160f2162940d6f
Shlumper <mushon4@gmail.com>
2021-04-14 17:01:28 +0300

    fix

--- start.sh
+++ start.sh
@@ -6,18 +6,18 @@ then
         exit
 fi
 
-echo "start fed on $ED_ENV"
+echo "start fed on $ED_ENV.."
 mkdir /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/db
-echo "createing db folder"
+echo "createing db folder.."
 echo "getting fed secret:"
 FED_KEY_NAME=`aws ec2 describe-instances --filters Name=instance-id,Values=$(wget -qO- http://instance-data/latest/meta-data/instance-id) --query Reservations[].Instances[].Tags[].Value --output text`
 FED_KEY=`aws secretsmanager get-secret-value --secret-id $FED_KEY_NAME --region us-east-2 | jq -r .SecretString`
 cat << EOF > /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/federator.key
 $FED_KEY
 EOF
-echo "starting federator"
+echo "starting federator:"
 nohup docker-compose up > federator.log 2>$1 &
-sleep 5
+sleep 30
 echo "federator logs: /home/ubuntu/Bridge-SC/federator.log"
 rm -rf /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/federator.key
 tail -f /home/ubuntu/Bridge-SC/federator.log
