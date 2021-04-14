c8d9ad41d073931a9659ee8a6b337589750b15a9
Shlumper <mushon4@gmail.com>
2021-04-14 17:04:51 +0300

    fix start scitp

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ echo "createing db folder.."
 echo "getting fed secret:"
 FED_KEY_NAME=`aws ec2 describe-instances --filters Name=instance-id,Values=$(wget -qO- http://instance-data/latest/meta-data/instance-id) --query Reservations[].Instances[].Tags[].Value --output text`
 FED_KEY=`aws secretsmanager get-secret-value --secret-id $FED_KEY_NAME --region us-east-2 | jq -r .SecretString`
+echo using key named: $FED_KEY_NAME
 cat << EOF > /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/federator.key
 $FED_KEY
 EOF
