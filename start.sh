4fdfb0c2b609dbcf2670826b50504ca01899d3db
Shlumper <mushon4@gmail.com>
2021-04-22 12:49:23 +0300

    remove__Get_key

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 export FED_ENV=$1
+export DEPLOY_ENV=$2
+
+cd "$(dirname "$0")"
 
 if [ -z "$FED_ENV" ]
 then
@@ -7,18 +11,33 @@ then
 fi
 
 echo "start fed on $ED_ENV.."
-mkdir /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/db
+mkdir federator-env/$FED_ENV/db
 echo "createing db folder.."
-echo "getting fed secret:"
-FED_KEY_NAME=`aws ec2 describe-instances --filters Name=instance-id,Values=$(wget -qO- http://instance-data/latest/meta-data/instance-id) --query Reservations[].Instances[].Tags[].Value --output text`
-FED_KEY=`aws secretsmanager get-secret-value --secret-id $FED_KEY_NAME --region us-east-2 | jq -r .SecretString`
-echo using key named: $FED_KEY_NAME
-cat << EOF > /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/federator.key
-$FED_KEY
+
+if [ "$DEPLOY_ENV" != "dev" ]
+then
+        echo "getting fed secret:"
+        FED_KEY_NAME=`aws ec2 describe-instances --filters Name=instance-id,Values=$(wget -qO- http://instance-data/latest/meta-data/instance-id) --query Reservations[].Instances[].Tags[].Value --output text`
+        FED_KEY=`aws secretsmanager get-secret-value --secret-id $FED_KEY_NAME --region us-east-2 | jq -r .SecretString`
+        echo using key named: $FED_KEY_NAME
+        cat << EOF > federator-env/$FED_ENV/federator.key
+        $FED_KEY
 EOF
+fi
 echo "starting federator please wait..."
-nohup docker-compose up > federator.log 2>$1 &
-sleep 30
-echo "federator logs: /home/ubuntu/Bridge-SC/federator.log"
-rm -rf /home/ubuntu/Bridge-SC/federator-env/$FED_ENV/federator.key
-tail -f /home/ubuntu/Bridge-SC/federator.log
+nohup docker-compose up > federator.log 2>&1 &
+
+
+
+if [ "$DEPLOY_ENV" != "dev" ]
+then
+        sleep 30
+        echo "federator logs: federator.log"
+        rm -rf federator-env/$FED_ENV/federator.key
+EOF
+fi
+
+tail -f federator.log
+
+
+cd -
