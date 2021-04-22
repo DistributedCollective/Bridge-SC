8881a495da1f172fbb6344904b2a2e85c471619b
Rainer Koirikivi <rainer@koirikivi.fi>
2021-04-22 22:34:25 +0300

    Add telegram config to config.sample.js

--- sovryn-token-bridge/federator/config/config.sample.js
+++ sovryn-token-bridge/federator/config/config.sample.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
+let telegramToken;
+try {
+    telegramToken = fs.readFileSync(`${__dirname}/telegram.key`, 'utf8').trim();
+} catch(e) {
+    console.debug(`Cannot load telegram token from ${__dirname}/telegram.key, bot disabled`);
+    telegramToken = '';
+}
 module.exports = {
     mainchain: require('./rsktestnet-kovan.json'), //the json containing the smart contract addresses in rsk
     sidechain: require('./kovan.json'), //the json containing the smart contract addresses in eth
@@ -6,6 +13,10 @@ module.exports = {
     confirmations: 120, // Number of blocks before processing it, if working with ganache set as 0
     privateKey: fs.readFileSync(`${__dirname}/federator.key`, 'utf8').trim(),
     storagePath: './db',
+    telegramBot: {
+        token: telegramToken,
+        groupId: 0, // Telegram group id to send the messages to
+    },
     confirmationTable: {
         "1": {
             "default": 5760,
