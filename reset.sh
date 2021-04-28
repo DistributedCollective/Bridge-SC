bf6fa4dce9fccd65720f919fe76a0807a3103b66
Shlumper <Shlumperx@gmail.com>
2021-04-28 11:56:46 +0300

    fix reset script

--- reset.sh
+++ reset.sh
@@ -1,3 +1,6 @@
+rm -rf federator-env/mainnet-BSC-RSK/db/
+rm -rf federator-env/mainnet-ETH-RSK/db/
+rm -rf federator-env/testnet-BSC-RSK/db/
 rm -rf federator-env/testnet-ETH-RSK/db/
 rm -rf federator.log
 git reset --hard
