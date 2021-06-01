ae42cf3fb75152825e718ece371b232ecea83634
bravepooh <brave.pooh8@gmail.com>
2021-06-01 10:01:30 +0300

    arrange files for deployment

--- sovryn-token-bridge/bridge/migrations/12_deploy_federation_v1.js
+++ sovryn-token-bridge/bridge/migrations/12_deploy_federation_v1.js
@@ -1,15 +1,17 @@
+const Federation = artifacts.require("Federation_v1");
 const Bridge = artifacts.require("Bridge_v0");
-const Bridge_v2 = artifacts.require("Bridge_v2");
 const MultiSigWallet = artifacts.require("MultiSigWallet");
 
 module.exports = function(deployer, networkName, accounts) {
     deployer
         .then(async () => {
+            const federation = await deployer.deploy(Federation, [accounts[0]], 1);
+
             const multiSig = await MultiSigWallet.deployed();
             const bridge = await Bridge.deployed();
-            const bridge_v2 = new web3.eth.Contract(Bridge_v2.abi, bridge.address);
-            
-            const endUpgradeData = bridge_v2.methods.endUpgrade().encodeABI();
-            await multiSig.submitTransaction(bridge.address, 0, endUpgradeData, { from: accounts[0] });
+
+            await federation.setBridge(bridge.address);
+            await federation.transferOwnership(multiSig.address);
+
         });
 };
---
+++ sovryn-token-bridge/bridge/migrations/17_deploy_federation_v2.js
@@ -0,0 +1,19 @@
+const Federation = artifacts.require("Federation");
+const Bridge = artifacts.require("Bridge_v0");
+const MultiSigWallet = artifacts.require("MultiSigWallet");
+
+module.exports = function(deployer, networkName, accounts) {
+    deployer
+        .then(async () => {
+            const federation = await deployer.deploy(Federation, [accounts[0]], 1);
+
+            const multiSig = await MultiSigWallet.deployed();
+            const bridge = await Bridge.deployed();
+
+            await federation.setBridge(bridge.address);
+            await federation.transferOwnership(multiSig.address);
+
+            const changeFederationData = bridge.contract.methods.changeFederation(federation.address).encodeABI();
+            await multiSig.submitTransaction(bridge.address, 0, changeFederationData, { from: accounts[0] });
+        });
+};
