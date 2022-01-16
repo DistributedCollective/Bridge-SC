7425577c3e56742c2c8e565b13ccc097fe604180
bravepooh <brave.pooh8@gmail.com>
2022-01-16 23:59:56 +0200

    update master start & reset scripts

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
-rm -rf federator-env/mainnet-BSC-RSK/db/
-rm -rf federator-env/mainnet-ETH-RSK/db/
-rm -rf federator-env/testnet-BSC-RSK/db/
-rm -rf federator-env/testnet-ETH-RSK/db/
+find federator-env/mainnet-BSC-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
+find federator-env/mainnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
+find federator-env/testnet-BSC-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
+find federator-env/testnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
 rm -rf federator.log
-git reset --hard origin/master
+git reset --hard origin/rinkeby-deploymnet
 git pull
