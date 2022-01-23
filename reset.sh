bac7dbe768c21c1156519e68b7c03f17d2911c01
bravepooh <brave.pooh8@gmail.com>
2022-01-23 16:44:17 +0200

    update reset.sh to master branch

--- reset.sh
+++ reset.sh
@@ -2,6 +2,7 @@ find federator-env/mainnet-BSC-RSK/db/. -type f -not -name 'failingTxIds.txt' -d
 find federator-env/mainnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
 find federator-env/testnet-BSC-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
 find federator-env/testnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
+find federator-env/rinkeby-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
 rm -rf federator.log
-git reset --hard origin/rinkeby-deploymnet
+git reset --hard origin/master
 git pull
