0a2a64e4b3b808e18244bb36138933e5cd2477da
Rainer Koirikivi <rainer@koirikivi.fi>
2022-09-07 13:44:51 +0300

    Reset script shenanigans

--- reset.sh
+++ reset.sh
@@ -5,4 +5,6 @@ find federator-env/testnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -d
 find federator-env/rinkeby-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
 rm -rf federator.log
 git fetch
-git reset --hard origin/signatures
+git reset --hard origin/master
+git checkout signatures
+git pull
