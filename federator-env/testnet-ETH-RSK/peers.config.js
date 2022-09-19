ad4d715ee613788bf9cc4b7f8d994013e9197776
Rainer Koirikivi <rainer@koirikivi.fi>
2022-09-19 21:55:27 +0300

    Update testnet ips

--- federator-env/testnet-ETH-RSK/peers.config.js
+++ federator-env/testnet-ETH-RSK/peers.config.js
@@ -1,17 +1,17 @@
 module.exports = {
     peers: [
         {
-            ip: '18.217.47.130',
+            ip: '3.16.182.143',
             port: 30303,
             address: '0x5fC4D8b1F96A916683954272721Cfe96ED5a3953',
         },
         {
-            ip: '3.144.219.93',
+            ip: '3.22.206.62',
             port: 30303,
             address: '0xCdC7Fe5A3bD1b8633B69ef6c47Dc9aD0c1c03ff8',
         },
         {
-            ip: '18.222.227.248',
+            ip: '52.14.2.117',
             port: 30303,
             address: '0x9805994fE19eB71c66Abb445d6bbcDE71635D8Dc',
         },
