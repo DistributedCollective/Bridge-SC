0fbbe3be79bd052dc735d1dcc7b415c5e29b63e0
Rainer Koirikivi <rainer@koirikivi.fi>
2022-10-06 09:45:07 +0300

    Actually remove the signatures scripts

--- reset_signatures.sh
+++
@@ -1,8 +0,0 @@
-find federator-env/mainnet-BSC-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
-find federator-env/mainnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
-find federator-env/testnet-BSC-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
-find federator-env/testnet-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
-find federator-env/rinkeby-ETH-RSK/db/. -type f -not -name 'failingTxIds.txt' -delete
-rm -rf federator.log
-git reset --hard origin/signatures
-git pull
